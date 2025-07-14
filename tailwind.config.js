1ab9ab5b976eedf588166500567e66d8a3ace4f0
yagmursabirli <yagmursabirli35@gmail.com>
2025-07-14 17:40:25 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        red400: 'hsl(7, 99%, 70%)',
+        yellow500: 'hsl(51, 100%, 49%)',
+        green800: 'hsl(167, 40%, 24%)', // graphic design text
+        blue800: 'hsl(198, 62%, 26%)',  // photography text
+        green500: 'hsl(167, 34%, 41%)', // footer 
+      },
+      backgroundImage:{
+        'header-image': "url('/images/desktop/image-header.jpg')",
+        'cherry-image': "url('/images/desktop/image-graphic-design.jpg')",
+        'orange-image': "url('/images/desktop/image-photography.jpg')",
+      }
+    },
+  },
+  plugins: [],
+}
+
